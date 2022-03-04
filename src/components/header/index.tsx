144fb54f25376df1801c9f48ed7b1109dd209dbd
Lei Hsu <ge159951@gmail.com>
2022-03-04 08:41:48 +0800

    refator : move header to layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -12,7 +12,7 @@ interface HeaderProps {
 
 const Header = () => {
   return (
-    <div className="absolute top-0 right-0 w-screen">
+    <div className="w-screen">
       <div className=" h-10 w-full bg-light-blue px-10"></div>
       <div className="flex h-16 w-full items-center justify-between bg-white px-10">
         {/* todo:logo image */}
