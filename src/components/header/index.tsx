fe94da4f6bc3e28e3bb1fd436669023e9faa83a9
Lei Hsu <ge159951@gmail.com>
2022-03-06 20:42:16 +0800

    feat : add home and pdplist page router

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import Button from '@Components/button';
 
@@ -11,12 +12,16 @@ interface HeaderProps {
 }
 
 const Header = () => {
+  const router = useRouter();
   return (
     <div className="max-w-screen">
       <div className=" h-10 w-full bg-light-blue px-10"></div>
       <div className="flex h-16 w-full items-center justify-between bg-white px-10">
         {/* todo:logo image */}
-        <div className="h-10 w-[300px] bg-international-orange"></div>
+        <div
+          className="h-10 w-[300px] cursor-pointer bg-international-orange"
+          onClick={() => router.push('/Home')}
+        ></div>
         <div className="flex h-10 w-[550px] items-center justify-start space-x-2 rounded-md border-2 border-platinum pl-4">
           <SearchOutlined className="text-azure" />
           <input placeholder="Search by product or SKU" className="w-full focus:outline-none" />
