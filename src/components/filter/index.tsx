3459780c4afc21c68476b5ef754351a5f619991d
Lei Hsu <ge159951@gmail.com>
2022-03-07 22:05:55 +0800

    feat : add filter but not finish yet

--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -1,15 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Checkbox } from 'antd';
 
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+
 interface FilterProps {
-  checkOptions: string[];
+  groupName: string;
+  checkOption: string[];
   checkValue: string[];
 }
+[];
+
+const mockData = [
+  {
+    groupName: 'Filter group',
+    checkOption: ['categories1', 'categories1', 'categories3'],
+    checkValue: [],
+  },
+  {
+    groupName: 'Filter group',
+    checkOption: ['categories4', 'categories5', 'categories6'],
+    checkValue: [],
+  },
+  {
+    groupName: 'Filter group',
+    checkOption: ['categories7', 'categories8', 'categories9'],
+    checkValue: [],
+  },
+];
+
+const FilterGroup = ({ mockData }) => {
+  const CheckboxGroup = Checkbox.Group;
+
+  const [openFilter, setOpenFilter] = useState<boolean>(true);
+
+  const OpenFilterIcon = () => {
+    return openFilter ? (
+      <UpOutlined style={{ color: '#1071FF' }} />
+    ) : (
+      <DownOutlined style={{ color: '#1071FF' }} />
+    );
+  };
+
+  return (
+    <>
+      {mockData &&
+        mockData?.map((item) => (
+          <div className="mb-10">
+            <div className="flex items-center justify-between">
+              <h3 className="text-h3">{item.groupName}</h3>
+              <div className="cursor-pointer" onClick={() => setOpenFilter((prev) => !prev)}>
+                <OpenFilterIcon />
+              </div>
+            </div>
+            <div className="w-20">
+              {openFilter && <CheckboxGroup options={item.checkOption} value={item.checkValue} />}
+            </div>
+          </div>
+        ))}
+    </>
+  );
+};
 
 const Filter = () => {
-  const CheckboxGroup = Checkbox.Group;
-  return <div>Filter</div>;
+  return (
+    <div>
+      <FilterGroup mockData={mockData} />
+    </div>
+  );
 };
 
 export default Filter;
--- src/components/page/productList/SideBar.tsx
+++ src/components/page/productList/SideBar.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import { Checkbox } from 'antd';
 
+import Filter from '@Components/filter';
+
 const subCateOptions = ['Subcategories1', 'Subcategories2', 'Subcategories3', 'Subcategories4'];
 
 const SideBar = () => {
@@ -18,7 +20,11 @@ const SideBar = () => {
           onChange={(value) => setSubCateValues(value)}
         />
       </div>
-      <div className="h-[650px] rounded-md bg-light-blue"></div>
+      <div className="h-[650px] rounded-md bg-light-blue py-10 px-6">
+        <h2 className="text-h2">Filters</h2>
+        <p className="font-bold text-main-blue">Clear all</p>
+        <Filter />
+      </div>
     </>
   );
 };
